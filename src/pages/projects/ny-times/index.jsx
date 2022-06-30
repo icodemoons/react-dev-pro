cc8f63c011de61821cb395734fcbd0370c4cdff9
M <m@Ms-MacBook-Air.lan>
2022-06-30 00:51:54 -0400

    if statment removal

--- src/pages/projects/ny-times/index.jsx
+++ src/pages/projects/ny-times/index.jsx
@@ -10,28 +10,7 @@ const NY_BOOKS_API_KEY = process.env.NEXT_PUBLIC_NY_BOOKS_API_KEY;
 
 export default function NyTimes() {
   const [type, setType] = useState("");
-  function renderType(type) {
 
-    if(type === "news") {
-      return    
-    <ApiResults
-    apiUrl={`https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=${NY_NEWS_API_KEY}`}
-    renderItem={(results) => <News {...results} />}
-    searchProp="title"
-    renderJson={(json) => json.results}
-  />
-    } else {  (type === "books")
-      return 
-       <ApiResults
-          apiUrl={`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${NY_BOOKS_API_KEY}`}
-          searchProp="title"
-          renderItem={(book) => <Book {...book} />}
-          renderJson={(json) => json.results.books}
-        />
-
-    }
-  }
-  
   return (
     <>
       <Container className=" text-black">
@@ -79,3 +58,28 @@ export default function NyTimes() {
 }
 
 
+function renderType() {
+  switch (type) {
+    case "news":
+      return (
+        <ApiResults
+          apiUrl={`https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=${NY_NEWS_API_KEY}`}
+          renderItem={(results) => <News {...results} />}
+          searchProp="title"
+          renderJson={(json) => json.results}
+        />
+      );
+    case "books":
+      return (
+        <ApiResults
+          apiUrl={`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${NY_BOOKS_API_KEY}`}
+          searchProp="title"
+          renderItem={(book) => <Book {...book} />}
+          renderJson={(json) => json.results.books}
+        />
+      );
+    default:
+      console.log("Sorry, we are out of " + type + ".");
+      return "";
+  }
+}
